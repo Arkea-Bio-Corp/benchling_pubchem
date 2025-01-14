09462917333752fff96cd4d0fb9a9a275e9d3ef2
Taylor Falk <tfalk@arkeabio.com>
2025-01-14 16:46:35 -0500

    ATD-3798 adding secret pulling to app

--- local_app/benchling_app/setup.py
+++ local_app/benchling_app/setup.py
@@ -1,6 +1,8 @@
-import os
+import json
+
 from functools import cache
-from pathlib import Path
+from boto3 import session as bt3sess
+from botocore.exceptions import ClientError
 
 from benchling_sdk.apps.framework import App
 from benchling_sdk.auth.client_credentials_oauth2 import ClientCredentialsOAuth2
@@ -13,15 +15,26 @@ def init_app_from_webhook(webhook: WebhookEnvelopeV0) -> App:
 
 
 @cache
+def get_secret():
+    """Use AWS Secrets to get app details"""
+    secret_name = "dev/benchling_pubchem"
+    region_name = "us-east-1"
+
+    session = bt3sess.Session()
+    client = session.client(service_name="secretsmanager", region_name=region_name)
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        raise e
+    return json.loads(get_secret_value_response["SecretString"])
+
+
+@cache
 def app_definition_id() -> str:
     # App definition ID is available to "global" apps. It uniquely identifies the Benchling App
     # above the tenant context.
-    #
-    # Although it is available via the webhook, for security purposes we choose to supply it with
-    # the App's code to avoid reusing elements of the webhook's data payload as part of its verification.
-    # For ease of setup, we retrieve it from an environment variable.
-    # You could choose to simply leave it in code like `return "appdef_SpzX0d5oDA"`.
-    app_def_id = os.environ.get("APP_DEFINITION_ID")
+    app_def_id = get_secret()["APP_DEFINITION_ID"]
     assert app_def_id is not None, "Missing APP_DEFINITION_ID from environment"
     return app_def_id
 
@@ -32,14 +45,13 @@ def _benchling_from_webhook(webhook: WebhookEnvelopeV0) -> Benchling:
 
 @cache
 def _auth_method() -> ClientCredentialsOAuth2:
-    client_id = os.environ.get("CLIENT_ID")
+    client_id = get_secret()["CLIENT_ID"]
     assert client_id is not None, "Missing CLIENT_ID from environment"
     client_secret = _client_secret_from_file()
     return ClientCredentialsOAuth2(client_id, client_secret)
 
 
 def _client_secret_from_file() -> str:
-    file_path = os.environ.get("CLIENT_SECRET_FILE")
-    assert file_path is not None, "Missing CLIENT_SECRET_FILE from environment"
-    with Path(file_path).open() as f:
-        return f.read()
+    client_secret = get_secret()["CLIENT_SECRET"]
+    assert client_secret is not None, "Missing CLIENT_SECRET from environment"
+    return client_secret
