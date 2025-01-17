f571b13e85701e705212363ac71f279dd19da2ff
Taylor Falk <tfalk@arkeabio.com>
2025-01-17 13:12:51 -0500

    ATD-3798 moving to a two lambda function infrastructure

--- local_app/app.py
+++ local_app/app.py
@@ -6,9 +6,11 @@ from local_app.benchling_app.setup import app_definition_id
 from local_app.lib.logger import get_logger
 
 logger = get_logger()
+logger.setLevel("INFO")
 
 
 def handler(event, context):
+    print(event)
     # For security, don't do anything else without first verifying the webhook
     app_def_id = app_definition_id()
 
@@ -17,4 +19,5 @@ def handler(event, context):
     logger.debug("Received webhook message: %s", event["body"])
 
     handle_webhook(json.loads(event["body"]))
-    return {"statusCode": 200, "body": "OK"}
+
+    return {"statusCode": 200, "body": json.dumps("App work completed.")}
